b3f139ede21b9f3eeea24aad3b7ab4821829f6d0
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-08-21 09:57:09 +0200

    ссс

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -11,23 +11,19 @@ class Modal extends Component {
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
   }
-
   componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeyDown);
   }
-
   handleKeyDown = e => {
     if (e.code === 'Escape') {
       this.props.onClose();
     }
   };
-
   handleBackdropClick = e => {
     if (e.target === e.currentTarget) {
       this.props.onClose();
     }
   };
-
   render() {
     const { largeImageURL } = this.props;
 
